9e31073d06d61280dac14b272ff9f1456e98d6ca
emarkoska <emarkoska92@gmail.com>
2019-10-24 00:15:17 +0200

    Function tokenizing text into sentences, words and plotting the frequency of words.

--- Exercise_01.py
+++ Exercise_01.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import nltk
 import numpy as np
 import pandas as pd
 
@@ -12,11 +14,26 @@ def read_corpus():
     return sentences, labels
 
 
+def plot_frequency():
+    sentences, labels = read_corpus()
+    flat_words_nonunique = []
+    for sentence in sentences:
+        flat_words_nonunique.append(nltk.word_tokenize(sentence.lower()))
+    flat_words_nonunique = np.concatenate(flat_words_nonunique).ravel()
+    flat_words = np.unique(flat_words_nonunique)
+    print(flat_words.shape)
+
+    frequency = nltk.probability.FreqDist(flat_words_nonunique)
+
+    top = list(frequency)[:20]
+    plt.plot(top)
+    plt.show()
+
+
 # ctrl+alt+l = indentation, formatting code
 
 def main():
-    sentences, labels = read_corpus()
-    print(sentences)
+    plot_frequency()
 
 
 if __name__ == '__main__':
