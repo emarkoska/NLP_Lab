58042fff8ba3a7183942b7a0368cd8fed951140f
emarkoska <emarkoska92@gmail.com>
2019-10-28 19:44:48 +0100

    Fixed bug in word corpus tokenized without punctuation. New function for plotting frequency x word for top 20 most frequent words.

--- Exercise_01.py
+++ Exercise_01.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import nltk
 import numpy as np
 import pandas as pd
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
 
 
 def read_corpus():
@@ -14,26 +16,45 @@ def read_corpus():
     return sentences, labels
 
 
-def plot_frequency():
+def plot_frequency_nonunique():
     sentences, labels = read_corpus()
     flat_words_nonunique = []
     for sentence in sentences:
+        # This tokenizer allows for punctuation to remain
         flat_words_nonunique.append(nltk.word_tokenize(sentence.lower()))
     flat_words_nonunique = np.concatenate(flat_words_nonunique).ravel()
     flat_words = np.unique(flat_words_nonunique)
-    print(flat_words.shape)
+    plot_top20mostfrequent(flat_words_nonunique)
 
-    frequency = nltk.probability.FreqDist(flat_words_nonunique)
 
-    top = list(frequency)[:20]
-    plt.plot(top)
-    plt.show()
-
+def plot_freq_nostopwordspunctuation():
+    tokenizer = RegexpTokenizer(r'\w+')
+    sentences, labels = read_corpus()
+    flat_words_nonunique = []
+    for sentence in sentences:
+        # This tokenizer allows for punctiation to remain
+        flat_words_nonunique.append(tokenizer.tokenize(sentence.lower()))
+    flat_words_nonunique = np.concatenate(flat_words_nonunique).ravel()
+    flat_words_noww = [x for x in flat_words_nonunique if
+                       x not in stopwords.words('english')]  # Removing the stopwords from the word list
+    plot_top20mostfrequent(flat_words_noww)
 
 # ctrl+alt+l = indentation, formatting code
 
+def plot_top20mostfrequent(dict):
+    frequency = nltk.probability.FreqDist(dict)  # Returns tuples of word : frequency
+    first20pairs = {k: frequency[k] for k in list(frequency)[:20]}  # Returns list of the first 20 pairs
+    sorted_x = sorted(first20pairs.items(), key=lambda kv: kv[1],
+                      reverse=True)  # Sorted in reverse so we have the most frequent words
+    x, y = zip(*sorted_x)  # Separating in two lists
+    plt.plot(x, y)
+    plt.show()
+
+
 def main():
-    plot_frequency()
+    plot_freq_nostopwordspunctuation()
+
+
 
 
 if __name__ == '__main__':
