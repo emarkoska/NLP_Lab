655cd048bf2eb8a3163a4b77eccd8816f08e22a0
emarkoska <emarkoska92@gmail.com>
2019-11-01 19:51:13 +0100

    Jupyter notebook.

--- Playground.py
+++ Playground.py
@@ -1,6 +1,61 @@
-def main():
-    print("a")
+import re
 
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+def clean_text(text):
+    # remove HTML tags
+    text = re.sub(r'<.*?>', '', text)
+
+    # remove the characters [\], ['] and ["]
+    text = re.sub(r"\\", "", text)
+    text = re.sub(r"\'", "", text)
+    text = re.sub(r"\"", "", text)
+
+    # convert text to lowercase
+    text = text.strip().lower()
+
+    # replace punctuation characters with spaces
+    filters = '!"\'#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n'
+    translate_dict = dict((c, " ") for c in filters)
+    translate_map = str.maketrans(translate_dict)
+    text = text.translate(translate_map)
+
+    return text
+
+
+def vectorized_todf():
+    training_texts = [
+        "This is a good cat",
+        "This is a bad day"
+    ]
+
+    test_texts = [
+        "This day is a good day"
+    ]
+
+    # this vectorizer will skip stop words
+    vectorizer = CountVectorizer(
+        stop_words="english",
+        preprocessor=clean_text
+    )
+
+    # fit the vectorizer on the training text
+    vectorizer.fit(training_texts)
+
+    # get the vectorizer's vocabulary
+    inv_vocab = {v: k for k, v in vectorizer.vocabulary_.items()}
+    vocabulary = [inv_vocab[i] for i in range(len(inv_vocab))]
+
+    # vectorization example
+    df = pd.DataFrame(
+        data=vectorizer.transform(test_texts).toarray(),
+        index=["test sentence"],
+        columns=vocabulary
+    )
+
+    print(df)
 
 if __name__ == '__main__':
-    main()
+    vectorized_todf()
